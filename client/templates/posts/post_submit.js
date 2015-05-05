e05fd3baba73718bed351cb14c93f112a2d9d679
alexchetv <alexchetv@gmail.com>
2015-05-05 21:38:45 +0500

    polymer button

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -8,32 +8,36 @@ Template.postSubmit.helpers({
   },
   errorClass: function (field) {
     return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  },
+  attributeObject:function(){
+    return {id: 'sun',
+      class:'btn-primary',
+      raised:true
+    }
   }
 });
 
 Template.postSubmit.events({
-  'submit form': function(e) {
-    e.preventDefault();
-    
+  'click #sun': function(e, template) {
     var post = {
-      url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val()
+      url: template.find('[name=url]').value,
+      title: template.find('[name=title]').value
     };
-    
+
     var errors = validatePost(post);
     if (errors.title || errors.url)
       return Session.set('postSubmitErrors', errors);
-    
+
     Meteor.call('postInsert', post, function(error, result) {
       // display the error to the user and abort
       if (error)
         return throwError(error.reason);
-      
+
       // show this result but route anyway
       if (result.postExists)
         throwError('This link has already been posted');
-      
-      Router.go('postPage', {_id: result._id});  
+
+      Router.go('postPage', {_id: result._id});
     });
   }
 });
